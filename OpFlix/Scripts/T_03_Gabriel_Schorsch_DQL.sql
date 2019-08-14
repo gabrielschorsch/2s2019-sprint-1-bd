a6c85c6bcc6296de3237d70d39a44176826eb0ec
Gabriel Schorsch Amadeu <49549303837@info.local>
2019-08-14 15:11:35 -0300

    Nothing to say here

--- OpFlix/Scripts/T_03_Gabriel_Schorsch_DQL.sql
+++ OpFlix/Scripts/T_03_Gabriel_Schorsch_DQL.sql
@@ -9,19 +9,26 @@ Select * from Plataformas
 Select DISTINCT Lancamentos.Titulo, Lancamentos.DataLancamento, Categoria.Nome As Categoria , Tipo.Nome As Tipo From Lancamentos 
 join Categoria on Lancamentos.IdCategoria = Categoria.IdCategoria join Tipo on Lancamentos.IdTipo = Tipo.IdTipo;
 
-Exec ListarFilmesPorCategoria 5
-Exec ContarFilmesPorCategoria 6
+Exec ListarFilmesPorCategoriaId 5
+Exec ListarFilmesPorCategoriaNome 'Ação'
+Exec ContarFilmesPorCategoriaId 6
+Exec ContarFilmesPorCategoriaNome 'Ação'
 
 Select * from vwGenerosPlataformas
 
 Select COUNT(*) AS QntdUsuarios from Usuarios
 
-CREATE PROCEDURE ListarFilmesPorCategoria @IdCategoria Int
+CREATE PROCEDURE ListarFilmesPorCategoriaId @IdCategoria Int
 AS Select * from Lancamentos Where IdCategoria = @IdCategoria;
 
+CREATE PROCEDURE ListarFilmesPorCategoriaNome @Categoria Varchar(15)
+AS Select * from Lancamentos join Categoria on Lancamentos.IdCategoria = Categoria.IdCategoria Where Categoria.Nome = @Categoria;
 
-CREATE PROCEDURE ContarFilmesPorCategoria @IdCategoria Int
+CREATE PROCEDURE ContarFilmesPorCategoriaId @IdCategoria Int
 AS Select COUNT(*) from Lancamentos Where IdCategoria = @IdCategoria
+
+CREATE PROCEDURE ContarFilmesPorCategoriaNome @Categoria Varchar(15)
+AS Select COUNT(*) from Lancamentos join Categoria on Lancamentos.IdCategoria = Categoria.IdCategoria Where Categoria.Nome = @Categoria
 
 
 CREATE VIEW vwGenerosPlataformas
